244baf59a77f301cd402a8bc5acbd4c69161c1ce
samela-comin <samela.a.g@gmail.com>
2025-09-12 19:49:49 +0000

    criação das variaveis e entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,48 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char estado;
+  char codigo [10];
+  char cidade [20];
+  int população;
+  float area;
+  float pib;
+  int turismo;
+
   // Área para entrada de dados
 
+  printf("Digite o Estado: \n");
+  scanf("%s", &estado);
+
+  printf("Digite o código da carta: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite a Cidade: \n");
+  scanf("%s", &cidade);
+
+  printf("Digite a população: \n");
+  scanf("%d", &população);
+
+  printf("Digite a área em km²: \n");
+  scanf("%f", &area);
+
+  printf("Digite o PIB: \n");
+  scanf("%f", &pib);
+
+  printf("Digite o número de pontos turísticos: \n");
+  scanf("%d", &turismo);
+
   // Área para exibição dos dados da cidade
+
+  //printf("Estado: %s\n",estado);
+  //printf("Código da carta: %s\n",codigo);
+  //printf("Nome da Cidade: %s \n", cidade);
+  //printf("População: %d \n",população);
+  //printf("Área em km²: %f \n", area);
+  //printf("PIB: %f \n",pib);
+  //printf("Número de pontos turísticos: %d \n",turismo);
+
+
 
 return 0;
 } 
